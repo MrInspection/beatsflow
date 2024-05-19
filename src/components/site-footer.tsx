95198ea5c10f6f4aceccd07796fae513e05ddc08
Moussa <moussaabdallahhoumadi@gmail.com>
2024-05-19 20:26:46 +0200

    Minor Tweak

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -5,7 +5,7 @@ export default function SiteFooter() {
                 <div className="container flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
                     <p className="text-balance text-center text-sm leading-loose text-muted-foreground md:text-left">
                         ©️ 2024 ProjectUltron, All rights reserved. Built by <a
-                        href={""}
+                        href={"https://github.com/mrinspection"}
                         target="_blank"
                         rel="noreferrer"
                         className="font-medium underline underline-offset-4"
