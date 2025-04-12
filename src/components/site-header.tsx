c5b979ebacb5b771a23506d12baeb8b8df7496ac
Moussa <moussaabdallahhoumadi@gmail.com>
2025-04-12 17:06:50 +0200

    chore: update next config for external images

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -1,9 +1,26 @@
+"use client"
+
+import {Aperture} from "lucide-react";
+import {ThemeToggle} from "@/components/theme-toggle";
+import {WorkflowPanelMobile} from "@/components/workflow/workflow-panel";
+import {MusicDialog} from "@/components/music/music-dialog";
+
 export default function SiteHeader() {
   return (
-    <header className="w-full">
-      <div className="h-14 flex items-center justify-center">
-        <h1 className="tracking-tight font-semibold text-base">Spectron BeatsFlōw.</h1>
-      </div>
-    </header>
+    <>
+      <header className="border-b sticky top-0 z-10">
+        <div className="px-6 flex h-14 items-center max-md:px-6 justify-between">
+          <div className="flex items-center gap-2">
+            <Aperture className="size-5"/>
+            <h1 className="font-bold tracking-tight">BeatsFlōw</h1>
+          </div>
+          <nav className="flex items-center">
+            <MusicDialog />
+            <WorkflowPanelMobile />
+            <ThemeToggle />
+          </nav>
+        </div>
+      </header>
+    </>
   )
 }
