7cff8bb146e377176bb44c6cbf5071b4676a73dd
Moussa <moussaabdallahhoumadi@gmail.com>
2026-05-03 13:54:42 +0200

    chore(deps): update dependencies

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { NuqsAdapter } from "nuqs/adapters/next";
 import type { PropsWithChildren } from "react";
 import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 
 export default function Providers({ children }: PropsWithChildren) {
@@ -12,7 +14,12 @@ export default function Providers({ children }: PropsWithChildren) {
       enableSystem
       disableTransitionOnChange
     >
-      <TooltipProvider delay={300}>{children}</TooltipProvider>
+      <NuqsAdapter>
+        <TooltipProvider delay={300}>
+          {children}
+          <Toaster position="top-center" />
+        </TooltipProvider>
+      </NuqsAdapter>
     </ThemeProvider>
   );
 }
